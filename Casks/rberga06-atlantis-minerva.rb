5036e1f81f7b506b5d60ad2f52ad1fea97dc1509
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2022-02-14 15:41:15 +0100

    Update rberga06-atlantis-minerva.rb

--- Casks/rberga06-atlantis-minerva.rb
+++ Casks/rberga06-atlantis-minerva.rb
@@ -4,7 +4,7 @@ cask "rberga06-atlantis-minerva" do
     version "09.16.03.10"
     sha256 "cf061a905195ec7cf49700ead7be132abf12a0fa688fa41724249598c6452fc7"
 
-    url "https://github.com/RBerga06/cern-atlantis-macapp/releases/download/v09.16.03.10/Atlantis-MINERVA.app.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/RBerga06/cern-atlantis-macapp/releases/download/v#{version}/Atlantis-MINERVA.app.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     name "CERN Minerva (Atlantis) - macOS Wrapper application"
     desc "Mastercalss INvolving Event Recognition Visualized with Atlantis"
 #    homepage "http://www.cern.ch/atlas-minerva"
