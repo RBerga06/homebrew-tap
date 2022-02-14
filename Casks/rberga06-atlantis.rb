8222d6f607df9fb7b901bdd8441429b92780905d
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2022-02-14 15:44:45 +0100

    Update rberga06-atlantis.rb

--- Casks/rberga06-atlantis.rb
+++ Casks/rberga06-atlantis.rb
@@ -2,7 +2,7 @@ require_relative "../lib/private_strategy"
 
 cask "rberga06-atlantis" do
     version "09.16.06.07"
-    sha256 ""
+    sha256 "9e45c7d9fbf372d89ac44dfab85ba3c642a8cef02e75224d23412d1e882c79e1"
 
     url "https://github.com/RBerga06/cern-atlantis-macapp/releases/download/v#{version}/Atlantis.app.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     name "CERN Atlantis - macOS Wrapper application"
