c4e4d04c654c3f3d3a20e8a45f4fcda7a876bea8
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2022-02-14 15:33:39 +0100

    Update rberga06-atlantis-minerva.rb

--- Casks/rberga06-atlantis-minerva.rb
+++ Casks/rberga06-atlantis-minerva.rb
@@ -3,12 +3,13 @@ require_relative "../lib/private_strategy"
 cask "rberga06-atlantis-minerva" do
     version "09.16.03.10"
     sha256 "cf061a905195ec7cf49700ead7be132abf12a0fa688fa41724249598c6452fc7"
-    
+
     url "https://github.com/RBerga06/cern-atlantis-macapp/releases/download/v09.16.03.10/Atlantis-MINERVA.app.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     name "CERN Minerva (Atlantis) - macOS Wrapper application"
     desc "Mastercalss INvolving Event Recognition Visualized with Atlantis"
-    homepage "http://www.cern.ch/atlas-minerva"
+#    homepage "http://www.cern.ch/atlas-minerva"
+    homepage "https://github.com/RBerga06/cern-atlantis-macapp"
 
-    app "Atlantis (MINERVA)"
+    app "Atlantis (MINERVA).app"
     depends_on cask: "temurin8"
 end
