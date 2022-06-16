654a7738008d2a7db8e1322fd96ba8624eb9414b
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2022-06-16 16:04:03 +0200

    Update rberga06-font-meslo-lgs-nf.rb

--- Casks/rberga06-font-meslo-lgs-nf.rb
+++ Casks/rberga06-font-meslo-lgs-nf.rb
@@ -4,7 +4,7 @@ cask "rberga06-font-meslo-lgs-nf" do
     version "1.0.0"
     #sha256 "f0630f93b2f8c27b0cda8c4a2bae2b7a67bdd70786500e109f38c3a9b145f523"
   
-    url "https://github.com/RBerga06/files-homebrew-tap/Fonts/MesloLGS%20NF.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/RBerga06/files-homebrew-tap/master/Fonts/MesloLGS%20NF.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     name "MesloLGS Nerd Font (enhanced for powerlevel10k)"
     #homepage "https://github.com/ryanoasis/nerd-fonts"
   
