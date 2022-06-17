f02ce8a6e9dcf7fa243f5a710bb883a716cf4366
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2022-06-17 14:29:42 +0200

    Update rberga06-fonts-runes.rb

--- Casks/rberga06-fonts-runes.rb
+++ Casks/rberga06-fonts-runes.rb
@@ -1,6 +1,8 @@
+require_relative "../lib/private_strategy"
+
 cask "rberga06-fonts-runes" do
     version "1.0.0"
-    url "https://github.com/RBerga06/files-homebrew-tap/master/Fonts/Runes.txt"
+    url "https://github.com/RBerga06/files-homebrew-tap/master/Fonts/Runes.txt"; :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     name "A collection of runic Fonts"
 
     depends_on cask: [
