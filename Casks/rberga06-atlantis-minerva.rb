ba2c5ea6fa433c25b92c655d9645ed8b81a02186
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-04-07 14:09:18 +0200

    Removed rberga06-firefox-developer-edition, improved code format for some casks

--- Casks/rberga06-atlantis-minerva.rb
+++ Casks/rberga06-atlantis-minerva.rb
@@ -1,16 +1,16 @@
 require_relative "../lib/private_strategy"
 
 cask "rberga06-atlantis-minerva" do
-    version "09.16.03.10"
-    sha256 "cf061a905195ec7cf49700ead7be132abf12a0fa688fa41724249598c6452fc7"
+  version "09.16.03.10"
+  sha256 "cf061a905195ec7cf49700ead7be132abf12a0fa688fa41724249598c6452fc7"
 
-    url "https://github.com/RBerga06/cern-atlantis-macapp/releases/download/v#{version}/Atlantis-MINERVA.app.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    name "CERN Minerva (Atlantis) - macOS Wrapper application"
-    desc "Mastercalss INvolving Event Recognition Visualized with Atlantis"
-#    homepage "http://www.cern.ch/atlas-minerva"
-    homepage "https://github.com/RBerga06/cern-atlantis-macapp"
+  url "https://github.com/RBerga06/cern-atlantis-macapp/releases/download/v#{version}/Atlantis-MINERVA.app.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  name "CERN Minerva (Atlantis) - macOS Wrapper application"
+  desc "Mastercalss INvolving Event Recognition Visualized with Atlantis"
+# homepage "http://www.cern.ch/atlas-minerva"
+  homepage "https://github.com/RBerga06/cern-atlantis-macapp"
+  
+  depends_on cask: "temurin8"
 
-    depends_on cask: "temurin8"
-
-    app "Atlantis (MINERVA).app"
+  app "Atlantis (MINERVA).app"
 end
--- Casks/rberga06-atlantis.rb
+++ Casks/rberga06-atlantis.rb
@@ -1,16 +1,17 @@
 require_relative "../lib/private_strategy"
 
 cask "rberga06-atlantis" do
-    version "09.16.06.07"
-    sha256 "9e45c7d9fbf372d89ac44dfab85ba3c642a8cef02e75224d23412d1e882c79e1"
+  version "09.16.06.07"
+  sha256 "9e45c7d9fbf372d89ac44dfab85ba3c642a8cef02e75224d23412d1e882c79e1"
 
-    url "https://github.com/RBerga06/cern-atlantis-macapp/releases/download/v#{version}/Atlantis.app.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    name "CERN Atlantis - macOS Wrapper application"
-    desc "An official event display for the ATLAS experiment at CERN's Large Hadron Collider."
-#    homepage "http://www.cern.ch/atlantis"
-    homepage "https://github.com/RBerga06/cern-atlantis-macapp"
+  url "https://github.com/RBerga06/cern-atlantis-macapp/releases/download/v#{version}/Atlantis.app.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  name "CERN Atlantis - macOS Wrapper application"
+  desc "An official event display for the ATLAS experiment at CERN's Large Hadron Collider."
+# homepage "http://www.cern.ch/atlantis"
+  homepage "https://github.com/RBerga06/cern-atlantis-macapp"
 
-    depends_on cask: "temurin8"
+  depends_on cask: "temurin8"
 
-    app "Atlantis.app"
+  app "Atlantis.app"
+
 end
