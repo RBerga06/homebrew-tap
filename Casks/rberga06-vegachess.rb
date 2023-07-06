2e1d7d551cfe784fe41c1f082ad29486d2bd31c9
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-07-06 15:17:46 +0200

    Added Vega chess cask

---
+++ Casks/rberga06-vegachess.rb
@@ -0,0 +1,11 @@
+cask "rberga06-vegachess" do
+    version "latest"
+    sha256 :no_check
+
+    url "http://www.vegachess.com/download/Vega.dmg"
+    name "Vega (chess)"
+    desc "A very intuitive and widespread software for chess tournaments management."
+    homepage "https://www.vegachess.com/"
+
+    app "Vega.app"
+  end
