e3f963ca616f45416132428fb9dbb174267bf319
Riccardo Bergamaschi <78449715+RBerga06@users.noreply.github.com>
2023-10-14 15:17:10 +0200

    Fix spacing

--- Casks/rberga06-vegachess.rb
+++ Casks/rberga06-vegachess.rb
@@ -1,11 +1,11 @@
 cask "rberga06-vegachess" do
-    version "latest"
-    sha256 :no_check
+  version "latest"
+  sha256 :no_check
 
-    url "http://www.vegachess.com/download/Vega.dmg"
-    name "Vega (chess)"
-    desc "A very intuitive and widespread software for chess tournaments management."
-    homepage "https://www.vegachess.com/"
+  url "http://www.vegachess.com/download/Vega.dmg"
+  name "Vega (chess)"
+  desc "Very intuitive and widespread software for chess tournaments management."
+  homepage "https://www.vegachess.com/"
 
-    app "Vega.app"
+  app "Vega.app"
 end
