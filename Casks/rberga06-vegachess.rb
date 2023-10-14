51931bfb3884c37a3eed23acbd926b73e2f5e04d
Riccardo Bergamaschi <78449715+RBerga06@users.noreply.github.com>
2023-10-14 16:05:16 +0200

    Description shouldn't end with a full stop.

--- Casks/rberga06-vegachess.rb
+++ Casks/rberga06-vegachess.rb
@@ -4,7 +4,7 @@ cask "rberga06-vegachess" do
 
   url "http://www.vegachess.com/download/Vega.dmg"
   name "Vega (chess)"
-  desc "Very intuitive and widespread software for chess tournaments management."
+  desc "Very intuitive and widespread software for chess tournaments management"
   homepage "https://www.vegachess.com/"
 
   app "Vega.app"
