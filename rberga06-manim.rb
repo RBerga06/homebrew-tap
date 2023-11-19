c63f1a58ee23e8349829ac4a5be9a1df4f524f91
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-11-19 20:48:02 +0100

    manim

--- rberga06-manim.rb
+++ rberga06-manim.rb
@@ -9,18 +9,39 @@ class RBerga06Manim < Formula
   url "https://files.pythonhosted.org/packages/2d/bc/71ac84b665f2d2f0d77f52bacb367224e04f7e5c24536beadcdc9147a0c2/manim-0.18.0-py3-none-any.whl"
   sha256 "56f598c66292d78ef11c56af54e06cf5203b8e227b76563f60177a2b4fa36719"
   license ""
+  # head "https://github.com/ManimCommunity/manim.git", branch: "main"
 
-  depends_on "python@3.12"
-  depends_on "py3cairo"
-  depends_on "ffmpeg"
+  # Dependencies
+  # System dependencies
   depends_on "pango"
+  depends_on "ffmpeg"
   depends_on "pkg-config"
-  depends_on "scipy"
   depends_on "basictex" => :recommended
+  # Python itself
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.12" => [:build, :test]
+  #depends_on "python@3.11" => [:build, :test]
+  # Python packages
+  depends_on "python-click"
+  depends_on "numpy"
+  depends_on "pillow"
+  depends_on "scipy"
+  depends_on "py3cairo"
+  depends_on "python-networkx"
+  depends_on "jupyterlab" => :optional
+  depends_on "pygments"
+
+  def pythons
+    deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
+  end
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    virtualenv_install_with_resources
+    # # ENV.deparallelize  # if your formula fails when building in parallel
+    # virtualenv_install_with_resources
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-m", "pip", "install", *std_pip_args, "."
+    end
   end
 
   test do
@@ -33,6 +54,11 @@ class RBerga06Manim < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    # system "false"
+
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", "import manim"
+    end
   end
 end
