a7366b81fa44af1b80283daff12d237509c1d526
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-11-20 00:08:51 +0100

    Remove comment

--- rberga06-manim.rb
+++ rberga06-manim.rb
@@ -10,7 +10,6 @@ class Rberga06Manim < Formula
 
   # Build/Test dependencies
   depends_on "python-setuptools" => :build
-  #depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
   # Runtime dependencies
   depends_on "ffmpeg"
