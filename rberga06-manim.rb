1730e3b8ca184e093b6e81924e798b996b08431e
RBerga06 <78449715+RBerga06@users.noreply.github.com>
2023-11-19 21:24:17 +0100

    Fix style

--- rberga06-manim.rb
+++ rberga06-manim.rb
@@ -1,9 +1,7 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
 class RBerga06Manim < Formula
   include Language::Python::Virtualenv
 
-  desc "A community maintained Python library for creating mathematical animations"
+  desc "Community maintained Python library for creating mathematical animations"
   homepage "https://www.manim.community/"
   url "https://files.pythonhosted.org/packages/2d/bc/71ac84b665f2d2f0d77f52bacb367224e04f7e5c24536beadcdc9147a0c2/manim-0.18.0-py3-none-any.whl"
   sha256 "56f598c66292d78ef11c56af54e06cf5203b8e227b76563f60177a2b4fa36719"
@@ -12,14 +10,13 @@ class RBerga06Manim < Formula
 
   # Dependencies
   # System dependencies
-  depends_on "pango"
   depends_on "ffmpeg"
+  depends_on "pango"
   depends_on "pkg-config"
-  depends_on "basictex" => :recommended
   # Python itself
-  depends_on "python-setuptools" => :build
   depends_on "python@3.12" => [:build, :test]
-  #depends_on "python@3.11" => [:build, :test]
+  depends_on "python-setuptools" => :build
+  # depends_on "python@3.11" => [:build, :test]
   # Python packages
   depends_on "python-click"
   depends_on "numpy"
@@ -27,8 +24,11 @@ class RBerga06Manim < Formula
   depends_on "scipy"
   depends_on "py3cairo"
   depends_on "python-networkx"
-  depends_on "jupyterlab" => :optional
   depends_on "pygments"
+  # Recommended dependencies
+  depends_on "basictex" => :recommended
+  # Optional dependencies
+  depends_on "jupyterlab" => :optional
 
   def pythons
     deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("python@") }
