adc228c15f778e9889e4f49a95863866e9f3eb67
Riccardo Bergamaschi <78449715+RBerga06@users.noreply.github.com>
2023-11-20 11:30:06 +0100

    Let's see how (and where) it's downloaded

--- rberga06-manim.rb
+++ rberga06-manim.rb
@@ -3,25 +3,25 @@ class Rberga06Manim < Formula
 
   desc "Community maintained Python library for creating mathematical animations"
   homepage "https://www.manim.community/"
-  url "https://files.pythonhosted.org/packages/06/62/7802597b93048a8ab543d835338739ce8f4e96e0410b63e29c86deb7cceb/manim-0.18.0.tar.gz"
-  sha256 "56788066bc1aec2471a988c91e337194fd38d1035ed1b4d10838bfe64bd26af8"
+  url "https://files.pythonhosted.org/packages/2d/bc/71ac84b665f2d2f0d77f52bacb367224e04f7e5c24536beadcdc9147a0c2/manim-0.18.0-py3-none-any.whl"
+  sha256 "56f598c66292d78ef11c56af54e06cf5203b8e227b76563f60177a2b4fa36719"
   license "MIT"
   head "https://github.com/ManimCommunity/manim.git", branch: "main"
 
   # Build/Test dependencies
-  depends_on "python-setuptools" => :build
+  # todo: depends_on "python-setuptools" => :build
   depends_on "python@3.12" => [:build, :test]
   # Runtime dependencies
   depends_on "ffmpeg"
-  depends_on "numpy"
+  # todo: depends_on "numpy"
   depends_on "pango"
-  depends_on "pillow"
+  # todo: depends_on "pillow"
   depends_on "pkg-config"
   depends_on "py3cairo"
-  depends_on "pygments"
-  depends_on "python-click"
-  depends_on "python-networkx"
-  depends_on "scipy"
+  # todo: depends_on "pygments"
+  # todo: depends_on "python-click"
+  # todo: depends_on "python-networkx"
+  # todo: depends_on "scipy"
   # Optional dependencies
   depends_on "jupyterlab" => :optional
 
@@ -32,8 +32,9 @@ class Rberga06Manim < Formula
   def install
     pythons.each do |python|
       python_exe = python.opt_libexec/"bin/python"
-      system python_exe, "-m", "pip", "install", *std_pip_args, "--upgrade", "pip"
-      system python_exe, "-m", "pip", "install", *std_pip_args, "poetry"
+      system "pwd"
+      system "ls", "."
+      system "false"
       system python_exe, "-m", "pip", "install", *std_pip_args, "."
     end
   end
